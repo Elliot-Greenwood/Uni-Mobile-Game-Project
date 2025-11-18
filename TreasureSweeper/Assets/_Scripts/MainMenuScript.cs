398873d1c2edcd888bbced20b492abd03d364ffa
Elliot-Greenwood <157498265+Elliot-Greenwood@users.noreply.github.com>
2025-11-18 16:16:03 +0000

    Menu Changes and new layout

---
+++ TreasureSweeper/Assets/_Scripts/MainMenuScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    
+    [SerializeField] GameObject ReturnButton;
+    [SerializeField] GameObject GameModeUI;
+    [SerializeField] GameObject LevelSelectionUI;
+
+
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+}
